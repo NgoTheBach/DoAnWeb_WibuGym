feb2a70a7c7f5faa56e1acf85e3a987051010f00
Bo1304 <88762546+Bo1304@users.noreply.github.com>
2022-06-18 22:29:57 +0700

    code moi

---
+++ DOAN_WEB_GYM/Controllers/ChiTietKhoaTapController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DOAN_WEB_GYM.Controllers
+{
+    public class ChiTietKhoaTapController : Controller
+    {
+        // GET: ChiTietKhoaTap
+        public ActionResult rpm_coure()
+        {
+            return View();
+        }
+
+        public ActionResult yoga_coure()
+        {
+            return View();
+        }
+
+        public ActionResult kickfit_coure()
+        {
+            return View();
+        }
+
+         public ActionResult bodypump_coure()
+        {
+            return View();
+        }
+        public ActionResult bodybalance_coure()
+        {
+            return View();
+        }
+
+        public ActionResult bodycompat_coure()
+        {
+            return View();
+        }
+    }
+}
