26af2202e3e8febc22367509a6a4077ec3a763a7
E7erlasting971 <91707431+E7erlasting971@users.noreply.github.com>
2022-06-24 22:40:17 +0700

    thanh

--- DOAN_WEB_GYM/Controllers/AdminController.cs
+++ DOAN_WEB_GYM/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DOAN_WEB_GYM.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,16 +9,48 @@ namespace DOAN_WEB_GYM.Controllers
 {
     public class AdminController : Controller
     {
-        // GET: Admin
+        
+        MyDataDataContext db = new MyDataDataContext();
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(FormCollection collection)
+        {
+            var tendn = collection["account"];
+            var matkhau = collection["password"];
+            if (String.IsNullOrEmpty(tendn))
+            {
+                ViewData["Loi1"] = "Phải nhập tên đăng nhập";
+            }
+            else if (String.IsNullOrEmpty(matkhau))
+            {
+                ViewData["Loi2"] = "Phải nhập mật khẩu";
+            }
+            else
+            {
+                TaiKhoanQTV ad = db.TaiKhoanQTVs.SingleOrDefault(n => n.Account == tendn && n.Password == matkhau);
+                if (ad != null)
+                {
+                    Session["Taikhoanadmin"] = ad;
+                    return RedirectToAction("ManHinhAdmin", "Admin");
+                }
+                else
+                    ViewBag.Thongbao = "Tên đăng nhập hoặc mật khẩu không đúng";
+            }
+            return RedirectToAction("Login", "Admin");
+        }
+        public ActionResult ManHinhAdmin()
+        {
+            return View();
+        }
         public ActionResult ThemKhoaTap()
         {
             return View();
         }
-        public ActionResult ManHinhAdmin()
-        {
-
-            return View();
-        }
+        
         public ActionResult ThemTinTuc()
         {
             return View();
@@ -38,9 +71,6 @@ namespace DOAN_WEB_GYM.Controllers
         {
             return View();
         }
-        public ActionResult Login()
-        {
-            return View();
-        }
+        
     }
 }
